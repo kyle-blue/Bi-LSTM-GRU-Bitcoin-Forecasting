86911865bfd7459e7491252de4cb769c103a46da
Kyle Doidge <kyle.blue.nuttall.09@gmail.com>
2021-01-28 18:10:15 +0000

    Changed dataset. Captured the dataset in an agglomerative dataframe. Done some preprocessing. To avoid preprocessing again added an automatic save/load feature

--- src/app/app.py
+++ src/app/app.py
@@ -10,22 +10,72 @@ def normalize(arr: np.array):
         x[...] = x / max_val
     return arr
 
+
+def get_main_dataframe():
+    PICKLE_NAME = "data.pkl"
+    PICKLE_FOLDER = f'{os.environ["WORKSPACE"]}/state/data'
+
+    data_folder = f'{os.environ["WORKSPACE"]}/data/normal_hours/hourly'
+    symbols = [x[:-4] for x in os.listdir(data_folder)] # Remove .csv file extension from strings
+    
+
+    if PICKLE_NAME in os.listdir(PICKLE_FOLDER):
+        main_df = pd.read_pickle(f"{PICKLE_FOLDER}/{PICKLE_NAME}")
+        print("Found existing dataframe, checking if it has correct number of columns (2x the number of symbols [1 for %_chg and 1 for volume])")
+        if len(main_df.columns) == 2 * len(symbols):
+            print(f"Dataframe found to be the most up to date, using dataframe in: {PICKLE_FOLDER}/{PICKLE_NAME}")
+            return main_df
+
+    main_df = pd.DataFrame()
+    for symbol in symbols:
+        filename = f"{data_folder}/{symbol}.csv"
+        df = pd.read_csv(filename, parse_dates=["Time"])
+        df.set_index("Time", inplace=True)
+        df = df[["%Chg", "Volume"]]
+
+        ## Convert %Chg column from string to float64
+        change_floats = [float(x[:-1]) for x in df["%Chg"]]
+        df["%Chg"] = change_floats
+
+        df.rename(columns={"%Chg": f"{symbol}_%_chg", "Volume": f"{symbol}_volume"}, inplace=True)
+        
+        if len(main_df) == 0: main_df = df
+        else: main_df = main_df.join(df, how="outer")
+
+        main_df.dropna(inplace=True)
+    
+    pd.to_pickle(main_df, f"{PICKLE_FOLDER}/{PICKLE_NAME}") # Save df to avoid future processing
+    return main_df
+
+
+
+
 def start():
     print("\n\n\n")
 
-    folder = os.environ["WORKSPACE"]
-    dataset_labels = pd.read_csv(f"{folder}/data/hourly/aapl_intraday-60min_historical-data-01-27-2021.csv", usecols=[0]).to_numpy()
-    dataset_values = pd.read_csv(f"{folder}/data/hourly/aapl_intraday-60min_historical-data-01-27-2021.csv", usecols=[1]).to_numpy()
-    dataset_values = normalize(dataset_values)
-    dataset = tf.data.Dataset.from_tensor_slices((dataset_values, dataset_labels))
+    main_df = get_main_dataframe()
+    print(f"MAIN DF")
+    print(main_df)
 
-    test_set = dataset.take(1000)
-    train_set = dataset.skip(1000)
 
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.LSTM(4))
-    model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(train_set, epochs=100, verbose=2)
+
+    # dataset_labels = pd.read_csv(f"{data_folder}/aapl_intraday-60min_historical-data-01-27-2021.csv", usecols=[0])
+    # dataset_values = pd.read_csv(f"{data_folder}/aapl_intraday-60min_historical-data-01-27-2021.csv", usecols=[1])
+
+
+
+
+
+    # dataset_values = normalize(dataset_values)
+    # dataset = tf.data.Dataset.from_tensor_slices((dataset_values, dataset_labels))
+
+    # test_set = dataset.take(1000)
+    # train_set = dataset.skip(1000)
+
+    # model = tf.keras.models.Sequential()
+    # model.add(tf.keras.layers.LSTM(4))
+    # model.add(tf.keras.layers.Dense(1, activation="sigmoid"))
+    # model.compile(loss='mean_squared_error', optimizer='adam')
+    # model.fit(train_set, epochs=100, verbose=2)
     # plt.plot(dataset)
     # plt.show()
