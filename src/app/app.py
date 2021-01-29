cce6a88226d15995cab15e8051da5de7b34cae5f
Kyle Doidge <kyle.blue.nuttall.09@gmail.com>
2021-01-29 14:02:12 +0000

    Added abs_avg column and target column to dataframe. Conducted data preprocessing (percent change and normalisation etc)

--- src/app/app.py
+++ src/app/app.py
@@ -1,8 +1,10 @@
+from typing import Deque
 from numpy.core.numeric import NaN
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from collections import deque
 import os
 
 def normalize(arr: np.array):
@@ -53,17 +55,43 @@ def get_main_dataframe():
 
 FUTURE_PERIOD = 4 # The look forward period for the future column, used to train the neural network to predict future price
 SEQUENCE_LEN = 100 # The look back period aka the sequence length. e.g if this is 100, the last 100 prices will be used to predict future price
-SYMBOL_TO_PREDICT = "MS" # The current symbol to train the model to base predictions on
+SYMBOL_TO_PREDICT = "GOOG" # The current symbol to train the model to base predictions on
+# The requirement for action (long or short) to be taken on a trade.
+# e.g. if this is 0.75, and the FUTURE_PERIOD is 4, 0.75*4 is 3. So the future % change must be 3* the avg abs % change to consider a long or short in the target column
+# The higher the number, the less trades (and less lenient), the lower the number, the more trades (and more lenient)
+ACTION_REQUIREMENT = 0.75
 
 def start():
     print("\n\n\n")
 
     main_df = get_main_dataframe()
 
-    ## Add future price column to main_df
+    
+
+    ## Create sequences
+    sequences = []
+    cur_sequence: Deque = deque(maxlen=SEQUENCE_LEN)
+
+    
+    ## Add abs_avg column (avg percent change over last x values)
+    ## The abs_avg is the absolute average % change over last x values
+    avgs = []
     symbol_data = main_df[f"{SYMBOL_TO_PREDICT}_%_chg"]
     symbol_data_len = len(symbol_data)
+    for i in range(symbol_data_len):
+        if i < SEQUENCE_LEN:
+            avgs.append(NaN)
+            continue
+        avgs.append(sum([abs(x) for x in symbol_data[i - SEQUENCE_LEN: i]]) / SEQUENCE_LEN)        
+
+    main_df["abs_avg"] = avgs
+    main_df.dropna(inplace=True)
+
+
+    ## Add future price column to main_df
     future = []
+    symbol_data = main_df[f"{SYMBOL_TO_PREDICT}_%_chg"]
+    symbol_data_len = len(symbol_data)
     for i in range(symbol_data_len):
         if i >= symbol_data_len - FUTURE_PERIOD:
             future.append(NaN)
@@ -74,11 +102,35 @@ def start():
     main_df.dropna(inplace=True)
 
     ## Add target column to main_df
+    targets = []
+    for i in range(len(main_df)):
+        multiplier_requirement = FUTURE_PERIOD * ACTION_REQUIREMENT
+        if abs(main_df["future"][i]) > (multiplier_requirement * main_df["abs_avg"][i]):
+            targets.append(1 if main_df["future"][i] >= 0 else 2) # Long trade target if positive, short trade target of negative
+        else:
+            targets.append(0)
+    main_df["target"] = targets
+    main_df.dropna(inplace=True)
+
+    ## Remove future price and abs_avg column, we don't need them anymore
+    main_df.drop(columns=["future", "abs_avg"], inplace=True)
+
+
+
+    ##### PREPROCESSING #####
+    for col in main_df.columns:
+        if "volume" in col: 
+            ## Change volumes into percent change also
+            main_df[col] = main_df[col].pct_change()
+            main_df.dropna(inplace=True)
+        if col != "target":
+            ## Normalise all data (except target price)
+            main_df[col] = normalize(main_df[col].values)
     
 
-    print(f"MAIN DF")
-    print(main_df)
 
+    print(f"\n\nMAIN DF FOR {SYMBOL_TO_PREDICT}")
+    print(main_df)
 
 
     # dataset_labels = pd.read_csv(f"{data_folder}/aapl_intraday-60min_historical-data-01-27-2021.csv", usecols=[0])
