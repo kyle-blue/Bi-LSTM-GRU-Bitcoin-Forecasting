f2b19061c07293bf04498f8444b32234a77243af
Kyle Doidge <kyle.blue.nuttall.09@gmail.com>
2021-04-03 17:05:36 +0100

    Made workspace environmnet variable dynamic

--- src/__main__.py
+++ src/__main__.py
@@ -5,6 +5,8 @@ def main():
     app.start()
 
 if __name__ == "__main__":
-    os.environ["WORKSPACE"] = "/home/doidge/trading_sltm"
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    work_dir = os.path.normpath(cur_dir + "/../")
+    os.environ["WORKSPACE"] = work_dir
     main()
     
