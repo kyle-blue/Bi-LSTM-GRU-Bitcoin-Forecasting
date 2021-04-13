0b7391c9089cc91f783321b3b2dbf15923d66339
Kyle Doidge <kyle.blue.nuttall.09@gmail.com>
2021-04-13 15:30:58 +0100

    Added correct fitness func

--- src/app/optimise_params.py
+++ src/app/optimise_params.py
@@ -86,7 +86,7 @@ def optimise_params(symbol: str, should_use_indicators: bool):
         return fitness
 
 
-    ga = GeneticAlgorithm(limits, maximisation_fitness_func,
+    ga = GeneticAlgorithm(limits, fitness_func,
         population_size=10, mutation_rate=0.2, generations=20,
         elitism=2, crossover_rate=0.9,
         log_file="results/params_optimisation.csv")
